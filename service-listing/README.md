c03a5e772fcda703a59d87f32a8312e88c9dc3f8
Sreya Sivakumar <sreyas@Sreyas-MacBook-Air.local>
2025-03-26 10:05:55 +0530

    update

--- service-listing/README.md
+++ service-listing/README.md
@@ -1,1 +1,2 @@
-deployment link=
+# FSWD_CA1
+https://67e3806a67eec8d304da830c--aquamarine-wisp-aacc1b.netlify.app/
--- service-listing/src/App.jsx
+++ service-listing/src/App.jsx
@@ -21,7 +21,7 @@ function App(){
               <h1>Services</h1>
               {Services.map((service) =>(
                 <ServiceCard key={service.id} service={service}/>
-              ))}
+              ))};
               
                 
             </div>
